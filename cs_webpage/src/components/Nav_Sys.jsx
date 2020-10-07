576fbe68813a1c6d075be94793aad01f8684a9f6
harjay12 <57065385+harjay12@users.noreply.github.com>
2020-10-07 07:53:04 -0400

    Process Components
    I set up React-Bootstrap and bootstrap for styling purposes. A folder component for the webpage has been created with multiple items such as Home, About, Event, Nav_Sys, and Contact that would be implemented later.

---
+++ cs_webpage/src/components/Nav_Sys.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Nav_Sys() {
+  return <div></div>;
+}
+
+export default Nav_Sys;
---
+++ cs_webpage/src/components/exporter.jsx
@@ -0,0 +1,7 @@
+import Home from "./Home";
+import About from "./About";
+import Event from "./Event";
+import Contact from "./Contact";
+import Nav_Sys from "./Nav_Sys";
+
+export { Home, About, Event, Contact, Nav_Sys };
