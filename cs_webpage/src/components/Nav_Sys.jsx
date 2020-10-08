98b4950f1e4076725d7f62c80cbde68d89d82172
harjay12 <57065385+harjay12@users.noreply.github.com>
2020-10-08 18:47:55 -0400

    Updated Nav_Sys
    The navbar system has been implemented.

--- cs_webpage/src/components/Nav_Sys.jsx
+++ cs_webpage/src/components/Nav_Sys.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
+import "./Nav_Sys.css";
 
-function Nav_Sys() {
-  return <div></div>;
+import "./navSystem.css";
+
+export default class Nav_Sys extends Component {
+  state = {
+    clicked: false,
+  };
+
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+  render() {
+    return (
+      <nav>
+        <div>
+          <div className="hamburger" onClick={this.handleClick}>
+            <i className={this.state.clicked ? "close" : "fas fa-bars"}></i>
+          </div>
+        </div>
+
+        <ul className={this.state.clicked ? "nav-links-active" : "nav-links"}>
+          <li>
+            <a href="/" exact>
+              Home
+            </a>
+          </li>
+          <li className="tue">
+            <a href="/foo#About">About</a>
+          </li>
+          <li>
+            <a href="/bar#Event">Event</a>
+          </li>
+          <li>
+            <a href="/bar#Contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
 }
-
-export default Nav_Sys;
